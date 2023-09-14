cc36ed704a7a60fabe3f0c18a8669d7a4da4b70e
Nick Payne <nick@kurai.co.uk>
2023-09-14 17:39:28 +0100

    Style up the basic 'Happy <day>!' OG image

--- app/hello-today/opengraph-image.tsx
+++ app/hello-today/opengraph-image.tsx
@@ -18,5 +18,22 @@ const daysOfWeek = [
 export default function Image() {
   const today = new Date();
   const dayName = daysOfWeek[today.getDay()];
-  return new ImageResponse(<div>Happy {dayName}!</div>, { ...size });
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 128,
+          background: "white",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        Happy {dayName}!
+      </div>
+    ),
+    { ...size }
+  );
 }
